521d56f56a1c556dea8bd8c1b34903bfd0389e8a
adimak212 <adimak212@gmail.com>
2025-09-11 01:12:06 -0700

    hello

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,12 @@
+import Message from '../components/massege'
+
+function homePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Message />
+    </div>
+  )
+}
+
+export default homePage
