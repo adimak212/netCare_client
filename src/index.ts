37a0c64be3801eaa811d2049ecc1c086be7011a6
adimak212 <adimak212@gmail.com>
2025-09-11 01:19:43 -0700

    fix

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import indexRouter from "./routes/index_router";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(cors({
+  origin: "http://localhost:5173",  // frontend dev server
+  methods: ["GET", "POST"],         // allowed methods
+  credentials: true                 // if you use cookies/auth
+}));
+
+// Use hello routes
+app.use("/", indexRouter);
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
