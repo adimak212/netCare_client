94a884603d8c49c876e08789d53cc19b126505dd
adimak212 <adimak212@gmail.com>
2025-09-12 04:46:26 -0700

    Removed unnecessary files

--- src/routes/index_router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import {getResponse} from "../controllers/getResponse";
-
-const router = Router();
-
-// GET /api/hello
-router.get("/getResponse", getResponse);
-
-export default router;
